9eed1e46b0ef4f85f5503118e749ac9a8d448840
PavelS <spavelik@gmail.com>
2017-12-21 23:11:47 +0700

    parse queary string.

--- src/net/Net.js
+++ src/net/Net.js
@@ -4,6 +4,20 @@ export default class Net {
 	//-----------------------/ Функции /---------------------------------------
 	constructor() {
 		this.oDataLoader = new DataLoader();
+		//parse query string
+		var urlParams;
+		var match,
+			pl     = /\+/g,  // Regex for replacing addition symbol with a space
+			search = /([^&=]+)=?([^&]*)/g,
+			decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+			query  = window.location.search.substring(1);
+	
+		urlParams = {};
+		while (match = search.exec(query))
+			urlParams[decode(match[1])] = decode(match[2]);
+		
+		this.params = urlParams;
+		console.log(this.params);
 	}
 
 	init(oGameData) {
