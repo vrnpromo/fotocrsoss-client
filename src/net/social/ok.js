4ea46443a7be92717bf693a660999b0485f20d48
PavelS <spavelik@gmail.com>
2018-01-24 13:48:32 +0700

    test ok.

--- src/net/social/ok.js
+++ src/net/social/ok.js
@@ -1,9 +1,31 @@
 export class OK {
+    constructor(){
+        var rParams = FAPI.Util.getRequestParameters();
+        FAPI.init(rParams["api_server"], rParams["apiconnection"],
+                /*
+                * Первый параметр:
+                * функция, которая будет вызвана после успешной инициализации.
+                */
+                function() {
+                    console.log("ok init");
+                    // здесь можно вызывать методы API
+                },
+                /*
+                * Второй параметр:
+                * функция, которая будет вызвана, если инициализация не удалась.
+                */
+                function(error) {
+                    console.log("ok init error");
+                }
+        );
+    }
     getFriends(){
-
+        return new Promise(function (resolve, reject) {
+            FAPI.Client.call({"fields":"name,pic50x50", "method":"friends.get"}, (resp) => { return resolve(resp.response)});
+        });
     }
 
     showInviteFriendsBox(){
-
+        FAPI.UI.showInvite("Поиграй в мою игру!");
     }
 }
