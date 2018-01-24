e8f041e4bdef63288f6930c54e8091daa22be0a0
PavelS <spavelik@gmail.com>
2018-01-24 14:22:25 +0700

    fix ok.

--- src/net/social/ok.js
+++ src/net/social/ok.js
@@ -21,7 +21,20 @@ export class OK {
     }
     getFriends(){
         return new Promise(function (resolve, reject) {
-            FAPI.Client.call({"fields":"name,pic50x50", "method":"friends.get"}, (resp) => { return resolve(resp.response)});
+            FAPI.Client.call({"method":"friends.getAppUsers"}, (resp) => { 
+                FAPI.Client.call({"method":"users.getInfo", "fields":"name,pic50x50,first_name,last_name", "uids":resp.uids.join(',')}, (r)=>{
+                    return resolve({
+                        count: r.length, 
+                        items:r.map(i => ({
+                            id:i.id,
+                            nickname: i.name, 
+                            photo_50:i.pic50x50,
+                            first_name: i.first_name,
+                            last_name: i.last_name,
+                        }))
+                    });
+                });
+            });
         });
     }
 
