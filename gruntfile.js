c142ecfd22ae9a54b2113082672f1120d5f4a81a
Piponga <djz@yandex.ru>
2018-10-16 17:42:22 +0300

    - ветка develop - livereload в gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,55 +1,56 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		browserify: {
-			main: {
-				options: {
-					browserifyOptions: {
-						debug: false
-					},
-					transform: [['babelify', {presets: ["es2015"]} ]] //{presets: ["stage-1", "es2015"]}
-				},
-				src: 'src/app.js',
-				dest: 'scripts/app.min.js'
-			}
-		},
-		watch: {
-			files: [ 'src/**/*.js' ],
-			tasks: ['browserify'],
-			options: {
-				spawn: false,
-			},
-		},
-		connect: {
-			target:{
-				options: {
-					port: 9001
-				}
-			}
-		},
-		bower: {
-			flat: {
-				dest: 'scripts',
-				options: {
-					debugging: true
-				}
-			}
-		},
-		uglify: {
-		    my_target: {
-		      files: {
-		        'scripts/app.min.js': ['scripts/app.min.js']
-		      }
-		    }
-		  }
-	});
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        browserify: {
+            main: {
+                options: {
+                    browserifyOptions: {
+                        debug: false
+                    },
+                    transform: [['babelify', {presets: ["es2015"]} ]] //{presets: ["stage-1", "es2015"]}
+                },
+                src: 'src/app.js',
+                dest: 'scripts/app.min.js'
+            }
+        },
+        watch: {
+            files: [ 'src/**/*.js' ],
+            tasks: ['browserify'],
+            options: {
+                spawn: false,
+                livereload: true
+            },
+        },
+        connect: {
+            target:{
+                options: {
+                    port: 9001
+                }
+            }
+        },
+        bower: {
+            flat: {
+                dest: 'scripts',
+                options: {
+                    debugging: true
+                }
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'scripts/app.min.js': ['scripts/app.min.js']
+                }
+            }
+        }
+    });
 
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-browserify');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	//grunt.loadNpmTasks('main-bower-files');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.loadNpmTasks('main-bower-files');
 
-	grunt.registerTask('default', [ 'connect', 'watch']); //'bower'
-	grunt.registerTask('deploy', [ 'browserify', 'uglify']);
+    grunt.registerTask('default', [ 'connect', 'watch']); //'bower'
+    grunt.registerTask('deploy', [ 'browserify', 'uglify']);
 };
